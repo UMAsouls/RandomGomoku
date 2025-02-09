d5b4417b2a4fbddb39dc8314c26fe70082b31163
takaryo1010 <117000270+takaryo1010@users.noreply.github.com>
2025-02-09 19:30:11 +0900

    勝率を表示させる

--- play.py
+++ play.py
@@ -72,29 +72,47 @@ print('Initial State:')
 #         else:
 #             print('second Win')
 #         break
-# minimax vs minimax
-env.render()
-while not done:
-    if env.current_player == 1:
-        print('minimax Agent')
-        action = minimax_agent.get_action(state,env.current_player)
-    else:
-        print('minimax Agent')
-        action = rule_based_agent.get_action(state,env.current_player)  # DQNエージェントのターン
-    print("state",state)
-    next_state, reward, done, info = env.step(action)
-    env.current_player = 3 - env.current_player
-    state = next_state
-    total_reward += reward
-    print('Total Reward:', total_reward)
-    print('Current State:')
+# minimax vs rule_based
+first_win = 0
+second_win = 0
+episodes = 50
+
+for episode in range(episodes):
+    
+    env = GomokuEnv.GomokuEnv(train_target="first" if episode % 2 == 0 else "second")
+
+    state = env.board.copy()
+    done = False
     env.render()
-    if done:
-        print('Game Over')
-        if env.current_player == 2:
-            print('first Win')
+    while not done:
+        if env.current_player == env.train_player:
+            print('minimax Agent')
+            action = minimax_agent.get_action(state,env.current_player)
         else:
-            print('second Win')
+            print('rule_based Agent')
+            action = rule_based_agent.get_action(state,env.current_player)  # DQNエージェントのターン
+        print("state",state)
+        next_state, reward, done, info = env.step(action)
+        env.current_player = 3 - env.current_player
+        state = next_state
+        total_reward += reward
+        print('Total Reward:', total_reward)
+        print('Current State:')
+        env.render()
+        if done:
+            print('Game Over')
+            if env.current_player == 2:
+                print('first Win')
+                first_win += 1
+                break
+            else:
+                print('second Win')
+                second_win += 1
+                break
+print(f"first win: {first_win}, first win rate: {first_win/episodes}  second win: {second_win}, second win rate: {second_win/episodes}")
+
+
+    
 
 
 print('Total Reward:', total_reward)
