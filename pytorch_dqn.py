47ab3c314882723be0d0c9dec712b46169693ef3
takaryo1010 <t.ryota1010@gmail.com>
2025-05-07 10:27:58 +0900

    畳み込みの有効化

--- pytorch_dqn.py
+++ pytorch_dqn.py
@@ -50,19 +50,19 @@ class ReplayBuffer:
 class QNet(nn.Module):
     def __init__(self, action_size):
         super(QNet, self).__init__()
-        # self.conv1 = nn.Conv2d(1, 32, kernel_size=3, padding=1)
-        # self.conv2 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
+        self.conv1 = nn.Conv2d(1, 32, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
         self.flatten = nn.Flatten()
-        self.fc1 = nn.Linear(64 * 19 * 19, 512)
-        self.fc2 = nn.Linear(512, action_size)
+        self.fc1 = nn.Linear(64 * 19 * 19, 256) # 512->256
+        self.fc2 = nn.Linear(256, action_size) # 512->256
 
     def forward(self, x):
-        x = x.unsqueeze(1)  # チャンネル次元追加
+        x = x.unsqueeze(1)
         x = F.relu(self.conv1(x))
         x = F.relu(self.conv2(x))
         x = self.flatten(x)
         x = F.relu(self.fc1(x))
-        x = torch.tanh(self.fc2(x))
+        x = self.fc2(x) # tanh -> none
         return x
 
 
